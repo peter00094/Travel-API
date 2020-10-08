f4f598d95401cc87192df4c9de94d963435e2cfd
DESKTOP-2A48IJL\USER <peter00094@gmail.com>
2020-10-09 00:27:33 +0800

    add more data and style

--- src/List.js
+++ src/List.js
@@ -12,12 +12,11 @@ export default class List extends Component {
         //解構取值
         const { data } = this.props;
         return (
-            <div style={{ width: "860px", height: "502px", margin: "0px auto" }}>
+            <div style={{ width: "860px", height: "302px", margin: "0px auto" }}>
                 {//將排序好的陣列印出
 
                     data.map(data => (
                         <Card key={data.id}>
-
                             <Headline6>{data.title}</Headline6>
                             <CardPrimaryContent className='demo-card__primary-action'>
                                 <div style={{ padding: "1rem" }} >
@@ -26,8 +25,19 @@ export default class List extends Component {
                                     {/* <CardMedia square  imageUrl={data.image_url}/> */}
                                     <Box src={data.image_url} alt=""></Box>
 
-                                    <p>評分 : {data.rating}</p>
-                                    <p>{data.tour_days}天 {data.min_price} 元起</p>
+                                    {/* 走訪物件中的陣列 將值一一回傳 */}
+                                    {data.tags.map(data=>{
+                                        return <Tag>{data}</Tag>
+                                    })}
+                                  
+                                    <br/>
+                                    {/* 走訪物件中的物件 將值一一回傳 */}
+                                    {data.group.map(data=>{
+                                        return(<Tag key={data.id}>{data.date.substr(5,8)} 可售 {data.quantity} 位</Tag>)
+                                    })}
+                                    
+                                    <p>評分 :<OrangeNum> {data.rating}</OrangeNum></p>
+                                    <OrangeNum>{data.tour_days}</OrangeNum>天 <OrangeNum>{data.min_price}</OrangeNum> 元起
 
                                 </div>
                             </CardPrimaryContent>
@@ -38,8 +48,27 @@ export default class List extends Component {
         )
     }
 }
-
+// 橘色大字
+const OrangeNum = styled.span`
+color: #ff9800;
+font-size: 23px;
+font-weight: 700;
+`
+//藍色標籤字
+const Tag = styled.p`
+　display: inline-block;
+    margin:10px 3px;
+    border-radius: 15px;
+    font-size: 14px;
+    padding: 0 7px;
+    color: #03a9f4;
+    background: #fff;
+    cursor: default;
+`
+//圖片大小
 const Box = styled.img`
+margin:0 auto;
+display:block;
 height:300px;
 width:300px;
 `
